fbf89c25535aae527a712b5383f315acb3fce30d
Aniket205kadam <aniketrkadam205@gmail.com>
2024-05-03 09:06:39 +0530

    Second week

---
+++ SpringJDBCExample2/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student(rollNo, name, marks) values (101, 'Rohit', 79);
+insert into student(rollNo, name, marks) values (102, 'Rohan', 89);
+insert into student(rollNo, name, marks) values (103, 'Mohan', 95);
